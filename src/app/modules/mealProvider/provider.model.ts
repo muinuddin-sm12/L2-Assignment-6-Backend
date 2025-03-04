c504c671c246f62def8d0e8421f68521f4993dcf
muinuddin-sm12 <muinuddin.sm12@gmail.com>
2025-03-04 22:47:01 +0400

    add: meal provider model

---
+++ src/app/modules/mealProvider/provider.model.ts
@@ -0,0 +1,16 @@
+import { model, Schema, Types } from "mongoose";
+import { IProvider } from "./provider.interface";
+
+const providerShcema = new Schema<IProvider>({
+    providerId: {type: Types.ObjectId, required: true},
+    providerName: {type: String, required:true},
+    cuisineSpecialties: {type: [String], required: true},
+    experience: {type: String, required: true},
+    pricingRange: {type: String, required: true},
+    averageRating: {type: Number, required: true},
+    availableDays: {type: [String], required: true},
+    availableTimeSlots: {type: String, required: true},
+    location: {type: String, required: true}
+})
+
+export const ProviderModel = model<IProvider>('Provider', providerShcema)
